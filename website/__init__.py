66c6e7899e561b641c582559b3f0ba3e02ebdce9
Ravi Singh <ravi149185@gmail.com>
2024-06-23 00:13:36 +0530

    feat: flask blueprint

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,14 @@
-a
+from flask import Flask
+from .admin import admin
+from .views import views
+from .auth import auth
+
+def create_app():
+  app = Flask(__name__)
+  app.config['SECRET_KEY'] = 'rtfgyhunjmklkmjnhbgvfcdxszsexdrcftvgybhnjmk'
+  
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/auth')
+  app.register_blueprint(admin, url_prefix='/admin')
+  
+  return app
